32cf88e30b0fc936544d44a025fe8ae4344e0eeb
BaretBuckley <thejocky10000@gmail.com>
2026-04-20 13:49:57 -0400

    simple image blurrer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use image::ImageReader;
+
+
 fn main() {
     println!("Hello, world!");
+
+    let img = match ImageReader::open("images/image.png") {
+        Ok(i) => match i.decode() {
+                Ok(decoded) => decoded,
+            Err(_) => {
+                println!("Failed to decode image\n");
+                return;
+            } 
+        },
+        Err(_) => {
+            println!("Failed to open image\n");
+            return;
+        }
+    };
+    let img2 = img.blur(5.0);
+    match img2.save("images/image-out.png") {
+        Ok(_) => {},
+        Err(_) => {
+            println!("Failed to save image\n");
+            return;
+        },
+    }
+
 }

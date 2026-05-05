506abd5aeaa2777254703ea8556f6a03664b1c3e
bproteau <bproteau@comcast.net>
2026-05-04 22:09:30 -0400

    Changed image_chunking a little.

--- src/image_chunking.rs
+++ src/image_chunking.rs
@@ -1,30 +1,35 @@
-use std::path::Path;
+use image::{DynamicImage, GenericImage, GenericImageView, SubImage, Rgba};
 
-use image::GenericImageView;
-use image::ImageError;
-use image::ImageReader;
-use image::Rgba;
-
-const CHUNK_SIZE : usize = 16;
-
-pub struct Chunk {
-    pub top_left : u32,
-    pub pix_data : Vec<Rgba<u8>>
+pub struct Chunk<'a> {
+    pub image: &'a DynamicImage,
+    pub coordinate: (u32, u32),
+    pub size: u32
 }
 
-pub fn img_to_chunks(img : &Path) -> Result<Vec<Chunk>, ImageError> {
-    let img = ImageReader::open(img)?.decode()?;
-    let width = img.width();
-    let pixels = img.pixels();
-    let mut chunks: Vec<Chunk> = vec![];
-    for (i, px) in pixels.enumerate() {
-        let chunk_idx = i%(width as usize)/CHUNK_SIZE;
-        if chunk_idx < chunks.len() {
-            chunks[chunk_idx].pix_data.push(px.2);
-        }
-        else {
-            chunks.push(Chunk { top_left: (i as u32), pix_data: (vec![]) });
-        }
+impl<'a> Chunk<'a> {
+    pub fn new(image: &'a DynamicImage, coordinate: (u32, u32), size : u32) -> Self {
+        Self { image, coordinate, size}
     }
-    Ok(chunks)
+
+    pub fn pixels(&self) -> Vec<Rgba<u8>> {
+        let mut out: Vec<Rgba<u8>>  = Vec::with_capacity((self.size*self.size) as usize);
+
+        let img_size = self.image.dimensions();
+
+        // If the 
+        if self.coordinate.0 > img_size.0 || self.coordinate.1 > img_size.1 {
+            return out
+        }
+
+        for p_y in self.coordinate.1 .. self.coordinate.1+self.size {
+            for p_x in self.coordinate.0 .. self.coordinate.0+self.size {
+                if p_x > img_size.0 || p_y > img_size.1 {
+                    return out;
+                } else {
+                    out.push(self.image.get_pixel(p_x, p_y));
+                }
+            } 
+        }
+        out
+    }
 }

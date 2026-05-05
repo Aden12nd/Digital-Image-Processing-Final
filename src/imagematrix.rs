afbe541bed0f82fb4fb867e1996ac752b478f1d9
Aden12nd <orangepuma06@gmail.com>
2026-05-04 21:30:06 -0400

    matrix creation

--- src/imagematrix.rs
+++ src/imagematrix.rs
@@ -21,6 +21,7 @@ pub fn create_matrix(width: u32, height: u32) -> DMatrix::<f64> {
     //let y: Vec<f64> = Vec::std::iter::&(0..width).iter().collect();
 
     let a = DMatrix::<f64>::from_column_slice(3 as usize, (width*height) as usize, &v);
+    eprintln!("{}", a);
     a
 
 }
@@ -28,26 +29,50 @@ pub fn create_matrix(width: u32, height: u32) -> DMatrix::<f64> {
 // degree = 0 : corresponds to features of just adding a constant a
 // degree = 1 : corresponds to features of bx + cy + a
 // degree = 2 : corresponds to featueres of 
-pub fn create_matrix_n(width: u32, height: u32) -> DMatrix::<f64> {
+// need to use smthn to do this
+pub fn create_matrix_n(width: u32, height: u32, n: u32) -> DMatrix::<f64> {
     
-    let mut v: Vec<f64> = Vec::with_capacity((height*width*(2)) as usize);
-    for n in 0..(height*width){
+    let mut v: Vec<f64> = Vec::with_capacity((height*width*((2*n+1) as u32)) as usize);
+    for j in 0..(height*width){
         v.push(1.0);
         println!("1");
     }
-    for n in 0..(height*width){
-        v.push((n/height) as f64);
-        println!("{0}", (n/height));
+
+    for deg in 1..=n {
+        for term in 0..=deg {
+            for g in 0..(height*width){
+                v.push((((g/height) as f64).powi((deg-term) as i32) * ((g % width) as f64).powi(term as i32)) as f64);
+                // println!("{0} ahhh", (((g/height).powi((deg-term) as i32) *(g % width)).powi(term as i32)) as f64);
+            }
+        }
     }
-    for n in 0..(height*width){
-        v.push((n % width) as f64);
-        println!("{0}", (n % width));
-    }
+
+    // for k in 1..(n+1){
+    //     for i in 0..(height*width){
+    //         v.push(((i/height).pow(k as u32)) as f64);
+    //         println!("{0}", ((i/height).pow(k as u32)));
+
+    //     }
+    //     if n > 1 {
+    //     //for deg in 2..(n+1){
+    //         for g in 0..(height*width){
+    //             v.push((((g/height).pow(k-1) *(g % width)).pow(k)) as f64);
+    //             println!("{0} ahhh", (((g/height).pow((k-1) as u32) *(g % width)).pow((k) as u32)));
+    //         }
+    //     //}
+    //     }
+    //     for p in 0..(height*width){
+    //         v.push(((p % width).pow(k as u32)) as f64);
+    //         println!("{0}", (p % width).pow(k as u32));
+    //     }
+        
+    // }
     //std::iter::&(0..height).collect();
     //let x: Vec<f64> = Vec::std::iter::&(0..height).iter().repeat(width).collect();
     //let y: Vec<f64> = Vec::std::iter::&(0..width).iter().collect();
 
-    let a = DMatrix::<f64>::from_column_slice(3 as usize, (width*height) as usize, &v);
+    let a = DMatrix::<f64>::from_column_slice((width*height) as usize, ((n + 1) * (n + 2) / 2) as usize, &v);
+    println!("{}", a);
     a
 
 }
--- src/main.rs
+++ src/main.rs
@@ -12,47 +12,48 @@ fn qt_builder(x: usize, y: usize) -> usize {
 }
 
 fn main() {
-    let test = dmatrix![1 as f64,1 as f64,1 as f64,1 as f64];
-    let testMat : DMatrix<f64> = imagematrix::matrixOp(imagematrix::create_matrix(2,2));
+    //let test = dmatrix![1 as f64,1 as f64,1 as f64,1 as f64];
+    let testMat : DMatrix<f64> = imagematrix::create_matrix_n(2,2,2);
+    //let testMat : DMatrix<f64> = imagematrix::matrixOp(imagematrix::create_matrix_n(2,2,2));
     println!("Hello, world!");
-    imagematrix::matVecMult(testMat, test);
-        let img = match ImageReader::open("images/image.png") {
-        Ok(i) => match i.decode() {
-                Ok(decoded) => decoded,
-            Err(_) => {
-                println!("Failed to decode image\n");
-                return;
-            } 
-        },
-        Err(_) => {
-            println!("Failed to open image\n");
-            return;
-        }
-    };
-    let img2 = img.blur(5.0);
-    match image_chunking::img_to_chunks(Path::new("images/image.png")) {
-        Ok (chunks_vec) => {
-            for (i, chunk) in chunks_vec.iter().enumerate() {
-                print!("Chunk {0}: \n{1}\n{2:?}\n", i, chunk.top_left, chunk.pix_data.len());
-            }
-        },
-        Err(_) => {
-            println!("You errored out, chud.");
-            return;
-        }
-    }
+    //imagematrix::matVecMult(testMat, test);
+    // let img = match ImageReader::open("images/image.png") {
+    //     Ok(i) => match i.decode() {
+    //             Ok(decoded) => decoded,
+    //         Err(_) => {
+    //             println!("Failed to decode image\n");
+    //             return;
+    //         } 
+    //     },
+    //     Err(_) => {
+    //         println!("Failed to open image\n");
+    //         return;
+    //     }
+    // };
+    // let img2 = img.blur(5.0);
+    // match image_chunking::img_to_chunks(Path::new("images/image.png")) {
+    //     Ok (chunks_vec) => {
+    //         for (i, chunk) in chunks_vec.iter().enumerate() {
+    //             print!("Chunk {0}: \n{1}\n{2:?}\n", i, chunk.top_left, chunk.pix_data.len());
+    //         }
+    //     },
+    //     Err(_) => {
+    //         println!("You errored out, chud.");
+    //         return;
+    //     }
+    // }
     
-    match img2.save("images/image-out1.png") {
-        Ok(_) => {},
-        Err(_) => {
-            println!("Failed to save image\n");
-            return;
-        },
-    }
+    // match img2.save("images/image-out1.png") {
+    //     Ok(_) => {},
+    //     Err(_) => {
+    //         println!("Failed to save image\n");
+    //         return;
+    //     },
+    // }
 
-    let quad_tree = QuadTree::QuadTree::newGrid(14, 17, qt_builder);
-    for i in quad_tree.iter() {
+    // let quad_tree = QuadTree::QuadTree::newGrid(14, 17, qt_builder);
+    // for i in quad_tree.iter() {
         
-    }
+    // }
 } 
 
